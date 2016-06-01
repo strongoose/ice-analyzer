207309b2e15493b262ace475cc990636e6bbed40
Daniel Strong <daniel.strong@digital.hmrc.gov.uk>
2016-06-01 18:49:55 +0100

    Split out subs and on-encounter effects

--- ice.py
+++ ice.py
@@ -20,5 +20,55 @@ def get_attribute_list(cards):
     return sorted(
         list(set(itertools.chain(*[card.keys() for card in cards]))))
 
-if __name__ == '__main__':
+def write_to_csv(cards, csv_file):
     pass
+
+def get_subs(ice):
+    '''
+    Return the list of subroutines of a piece of ice.
+    '''
+    return [line for line in ice["text"].split("\n")
+            if line.startswith("[Subroutine]")]
+
+def get_on_encounter(ice):
+    return [line for line in ice["text"].split("\n")
+            if line.startswith("When the Runner encounters")]
+
+def explode_subs(ice_list):
+    '''
+    Take a cards dictionary and adds the same number of subroutine
+    attributes to all of them (equal to the max possible number of subs.
+    Extra sub fields are blank. Modifies dictionary in place.
+    '''
+    most_subs = max([len(get_subs(ice)) for ice in ice_list])
+    for ice in ice_list:
+        subs = get_subs(ice)
+        for index in range(0, most_subs):
+            if index < len(subs):
+                ice["subroutine{}".format(index + 1)] = subs[index]
+            else:
+                ice["subroutine{}".format(index + 1)] = ""
+
+def explode_on_encounter(ice_list):
+    '''
+    Set an on_encounter_effect attribute on each ice in the list. This
+    does assume that each ice only has one on encounter effect, which is
+    true to date and seems likely to remain true (famous last words?).
+    '''
+    for ice in ice_list:
+        on_encounter = get_on_encounter(ice)
+        if on_encounter:
+            ice["on_encounter_effect"] = on_encounter[0]
+        else:
+            ice["on_encounter_effect"] = ""
+
+
+if __name__ == '__main__':
+
+    ices = get_ice_list()
+
+    explode_subs(ices)
+
+    explode_on_encounter(ices)
+
+    # write to csv file
